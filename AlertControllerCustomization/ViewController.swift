9e1aff003b0ef521651b92a1ce04cfb395d59c2d
Maxim Bilan <maximb.mail@gmail.com>
2016-02-20 12:49:42 +0200

    Modified view conrtoller Added alert controller example

--- AlertControllerCustomization/ViewController.swift
+++ AlertControllerCustomization/ViewController.swift
@@ -12,14 +12,29 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		let rightBarButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Done, target: self, action: "doneAction")
+		self.navigationItem.rightBarButtonItem = rightBarButton
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	func doneAction() {
+		let alertController = UIAlertController(title: "Title", message: "Message", preferredStyle: UIAlertControllerStyle.ActionSheet)
+		
+		let alertAction1 = UIAlertAction(title: "One", style: UIAlertActionStyle.Default, handler: nil)
+		let alertAction2 = UIAlertAction(title: "Two", style: UIAlertActionStyle.Default, handler: nil)
+		let alertAction3 = UIAlertAction(title: "Three", style: UIAlertActionStyle.Default, handler: nil)
+		let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+		
+		alertController.addAction(alertAction1)
+		alertController.addAction(alertAction2)
+		alertController.addAction(alertAction3)
+		alertController.addAction(cancelAction)
+		
+		let popPresenter = alertController.popoverPresentationController
+		popPresenter?.sourceView = self.view
+		popPresenter?.barButtonItem = self.navigationItem.rightBarButtonItem
+		self.presentViewController(alertController, animated: true, completion: nil)
 	}
-
 
 }
 
