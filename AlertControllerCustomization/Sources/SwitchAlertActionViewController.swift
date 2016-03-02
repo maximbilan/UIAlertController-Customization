32c7636b1bf6544986497b7b845bac8420e3d475
Maxim Bilan <maximb.mail@gmail.com>
2016-03-02 17:51:48 +0200

    Added Switch Alert View class

---
+++ AlertControllerCustomization/Sources/SwitchAlertActionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SwitchAlertActionViewController.swift
+//  Bitsboard-Telling Time
+//
+//  Created by Maxim Bilan on 2/16/16.
+//
+//
+
+import UIKit
+
+class SwitchAlertActionViewController: UIViewController {
+
+	@IBOutlet weak var valueSwitch: UISwitch!
+	var isSwitchOn = false
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		
+		valueSwitch.on = isSwitchOn
+    }
+	
+}
